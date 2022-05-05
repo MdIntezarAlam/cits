da743cb6a0796df6fa94882878e0ebdd2cc2cec0
mdintezaralam <mintezar99@gmail.com>
2022-05-05 22:12:37 +0530

    first

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtayveOQBhXw5fqejSUZGW_Ww4sxO8Mgg",
-  authDomain: "tidy-hold-337408.firebaseapp.com",
-  projectId: "tidy-hold-337408",
-  storageBucket: "tidy-hold-337408.appspot.com",
-  messagingSenderId: "188094564874",
-  appId: "1:188094564874:web:947d9af319c87e2ce4adf3"
+  apiKey: "AIzaSyCrCjSSHcpIzRI3Jc2ltDaSps9dliKBmJE",
+  authDomain: "citsres.firebaseapp.com",
+  projectId: "citsres",
+  storageBucket: "citsres.appspot.com",
+  messagingSenderId: "544996882934",
+  appId: "1:544996882934:web:5ea72cfa33795421efafd7"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
-
 
